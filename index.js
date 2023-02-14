c655943daa3d8da5ed572379a771ebd2f0aa0690
Sheikh sadi <sheikhsadi1620@gmail.com>
2023-02-14 16:56:52 +0600

    schedule data updated

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const projects = require('./data/schedule.json')
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
+
 app.get('/', async(req, res)=>{
      res.send('server is running')
+})
+
+app.get('/schedule', async(req, res)=>{
+     res.send(projects)
 })
 
 app.listen(port,()=>{
